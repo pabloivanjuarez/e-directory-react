a5d1dfed3a6f5af6b782c4a5cc8690bd65a3eb5c
pablito <pabloivanjuarez@gmail.com>
2020-11-22 19:14:30 -0800

    fs update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Start from './pages/start';
+
+function App() {
+  return (
+    <div>
+      <Start />
+    </div>
+  );
+}
+
+export default App;

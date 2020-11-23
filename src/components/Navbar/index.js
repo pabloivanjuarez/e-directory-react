20433930c2b1d359c5537a5714cfa2c07e3261e8
pablito <pabloivanjuarez@gmail.com>
2020-11-22 20:35:52 -0800

    title created for header section

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './style.css';
 
+function Header() {
+  return (<div className='header text-center'>
+    <h1>Employee Directory</h1>
+  </div>)
+}
 
-
-
-
-
-export default
+export default Header;

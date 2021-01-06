ccbfc09d9d0752eff6fd40ed769ec2049eeac772
pablito <pabloivanjuarez@gmail.com>
2021-01-05 23:05:54 -0800

    path cleared for search bar and employee row

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import SearchBar from "../SearchBar/index.js";
-import EmpRows from "../EmpRows/index.js";
+import SearchBar from "../SearchBar/index";
+import EmpRows from "../EmpRows/index";
 import "./style.css";
 import API from "../../utils/API";
 

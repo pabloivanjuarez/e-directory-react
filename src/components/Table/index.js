0185c414c78a87ccb81eb15a0db653f3ff23082f
pablito <pabloivanjuarez@gmail.com>
2021-01-06 01:59:45 -0800

    path updated, for dependencies

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import SearchBar from "../SearchBar/";
-import EmpRows from "../EmpRows/";
+import SearchBar from "../SearchBar";
+import EmpRows from "../EmpRows";
 import "./style.css";
 import API from "../../utils/API";
 
